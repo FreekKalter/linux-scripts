fcd81741cc8cb30544dfcdb8dfacba2c93c62b41
Freek Kalter <freek@kalteronline.org>
2013-10-23 11:08:51 +0200

    x11-diag: diagnose x11 too many peers error

---
+++ x11-diag.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from subprocess import Popen, PIPE
+
+client_sockets = []
+
+ns = Popen(["netstat", "-an", "--unix"], stdout=PIPE)
+output = ns.communicate()[0]
+for line in output.split('\n'):
+	if line.find("X11-unix") == -1 and len(line.split()) > 6 :
+        inode = line.split()[6]
+        client_sockets.append(inode)
+
+lsof = Popen(["lsof", "-U", "+c0", "-w"], stdout=PIPE)
+output = lsof.communicate()[0]
+for line in output.split('\n'):
+	try:
+		inode = line.split()[7]
+		if inode in client_sockets:
+			print line
+	except:
+		pass
