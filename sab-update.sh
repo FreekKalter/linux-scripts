bd6b1cb0f45e809f6db28fa46a4e71eaa92ec571
Freek Kalter <freek@kalteronline.org>
2013-10-25 20:40:25 +0200

    better error handling and reporting

--- sab-update.sh
+++ sab-update.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-set -e
-set -o pipefail
+#set -e
+#set -o pipefail
 
-stop sabnzbd
-sleep 20
+echo "[-] stopping sabnzd"
+stop sabnzbd && /home/fkalter/scripts/sleep-progress 20
+echo "[+] sabnzd stopped"
 
 cd /tmp/
-wget http://sourceforge.net/projects/sabnzbdplus/files/latest/download\?source\=dlp -O sabnzbd.tar.gz
+echo "[-] downloading latest version"
+wget -q http://sourceforge.net/projects/sabnzbdplus/files/latest/download\?source\=dlp -O sabnzbd.tar.gz || exit 1
+echo "[+] downloading done"
 
 FOLDER=`tar -xvzf sabnzbd.tar.gz | sed 's/\/.*//' | tail -n1`
 rm sabnzbd.tar.gz
+echo "[-] extracting update"
 mv $FOLDER /usr/local/bin
+if [[ $? -ne 0 ]]; then
+    start sabnzbd || echo "[!] something went wrong: could not start sabnzbd"; exit 1
+    rm -rf $FOLDER
+    echo "already up to date"
+    exit 0
+fi
+echo "[+] extracted"
 
 cd /usr/local/bin
 chown fkalter:fkalter $FOLDER
 rm SABnzbd
 ln -s /usr/local/bin/$FOLDER /usr/local/bin/SABnzbd
+echo "[+] links created"
 
-start sabnzbd
+echo "[+] restarting sabnzb"
+start sabnzbd || echo "[!] something went wrong: could not start sabnzbd"; exit 1
+echo "[+] Done: updated to ${FOLDER}"
