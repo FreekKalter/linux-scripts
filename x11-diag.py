b1c504095bc14e234b2038ad90d110d33d1ebdc4
Freek Kalter <freek@kalteronline.org>
2013-11-05 23:47:24 +0100

    diagnose Xserver "to many connections" error

--- x11-diag.py
+++ x11-diag.py
@@ -7,16 +7,16 @@ client_sockets = []
 ns = Popen(["netstat", "-an", "--unix"], stdout=PIPE)
 output = ns.communicate()[0]
 for line in output.split('\n'):
-	if line.find("X11-unix") == -1 and len(line.split()) > 6 :
+    if line.find("X11-unix") == -1 and len(line.split()) > 6 :
         inode = line.split()[6]
         client_sockets.append(inode)
 
 lsof = Popen(["lsof", "-U", "+c0", "-w"], stdout=PIPE)
 output = lsof.communicate()[0]
 for line in output.split('\n'):
-	try:
-		inode = line.split()[7]
-		if inode in client_sockets:
-			print line
-	except:
-		pass
+    try:
+        inode = line.split()[7]
+        if inode in client_sockets:
+            print line
+    except:
+        pass
