1b298d64a9708be97be34796760d05fe64446138
Freek Kalter <freek@kalteronline.org>
2014-04-07 19:57:12 +0200

    use new and improved tsr

--- backup.sh
+++ backup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+TARSNAP=/usr/local/bin/tarsnap
+HOME=/home/fkalter
+TSR=$HOME/gopath/bin/tsr
+LOG=/var/log/tarsnap
 
-/home/fkalter/scripts/tarsnap-go -c --configfile /home/fkalter/.tarsnaprc --aggressive-networking --print-stats -X /home/fkalter/.tarsnap-home-exclude -f home /home/fkalter --nr-backups 3
-/home/fkalter/scripts/tarsnap-go -c --configfile /home/fkalter/.tarsnaprc --aggressive-networking --print-stats -X /home/fkalter/.tarsnap-root-exclude -f root / --nr-backups 3
+if [[ $1 == "root" ]]; then
+    echo "`date`------------------------------------------------------------------------------" >> $LOG/log.log
+    $TARSNAP --configfile $HOME/.tarsnaprc -c -f "log(`$TSR --time`)" /var/log &>> $LOG/log.log
+    $TSR --configfile $HOME/.tarsnaprc --delete log &>> $LOG/log.log
+
+    echo "`date`------------------------------------------------------------------------------" >> $LOG/etc.log
+    $TARSNAP --configfile $HOME/.tarsnaprc -c -f "etc(`$TSR --time`)" /etc  &>> $LOG/etc.log
+    $TSR --configfile $HOME/.tarsnaprc --delete etc &>> $LOG/etc.log
+else
+    echo "`date`------------------------------------------------------------------------------" >> $LOG/home.log
+    $TARSNAP --configfile $HOME/.tarsnaprc -c -f "home(`$TSR --time`)" $HOME &>> $LOG/home.log
+    $TSR --configfile $HOME/.tarsnaprc --delete home &>> $LOG/home.log
+fi
