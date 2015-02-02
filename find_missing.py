9b91bb7caabcd5a6634636186073f0ef259a31ba
Freek Kalter <freek@kalteronline.org>
2015-02-02 14:22:12 +0100

    Added --include_dirs argument
    commandline argument to include dirs in the searched for files

--- find_missing.py
+++ find_missing.py
@@ -6,10 +6,14 @@ import sys
 parser = argparse.ArgumentParser(description="find missing files in a series of numbered files")
 parser.add_argument('--path', default='.', help='directory to look for the serie')
 parser.add_argument('--file_pattern', help='file pattern to match files againts ex: *.txt, help_*')
+parser.add_argument('--include_dirs', action='store_true', help='include directories in files to match')
 args = parser.parse_args()
-d = Path(args.path)
 
-serie = [int(re.search('\d+', f.name).group(0))for f in d.files(args.file_pattern)]
+d = Path(args.path)
+file_list = d.files(args.file_pattern)
+if args.include_dirs:
+	file_list += d.dirs()
+serie = [int(re.search('\d+', f.name).group(0)) for f in file_list]
 try:
 	not_in_serie = [i for i in range(min(serie),max(serie)) if i not in serie]
 except ValueError:
