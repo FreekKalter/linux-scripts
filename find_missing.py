f82d8ac7045516f8c38158d2771affb6b9a59334
Freek Kalter <freek@kalteronline.org>
2015-02-02 12:48:20 +0100

    Add commandline arguments to find_missing
    specify path and filename pattern on commandline

--- find_missing.py
+++ find_missing.py
@@ -1,9 +1,19 @@
 from path import Path
+import argparse
 import re
+import sys
 
-d = Path('.')
-serie = [int(re.search('\d+', f.name).group(0))for f in d.files('*.txt')]
-not_in_serie = [i for i in range(min(serie),max(serie)) if i not in serie]
+parser = argparse.ArgumentParser(description="find missing files in a series of numbered files")
+parser.add_argument('--path', default='.', help='directory to look for the serie')
+parser.add_argument('--file_pattern', help='file pattern to match files againts ex: *.txt, help_*')
+args = parser.parse_args()
+d = Path(args.path)
+
+serie = [int(re.search('\d+', f.name).group(0))for f in d.files(args.file_pattern)]
+try:
+	not_in_serie = [i for i in range(min(serie),max(serie)) if i not in serie]
+except ValueError:
+	sys.exit(0)
 
 for i in not_in_serie:
 	print '{} not in serie'.format(i)
