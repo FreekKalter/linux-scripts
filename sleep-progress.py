704a4fafde6c089eaf5f937dfeec5afeb7917aef
Freek Kalter <freek@kalteronline.org>
2015-03-25 16:12:05 +0100

    implemented a better time parser

--- sleep-progress.py
+++ sleep-progress.py
@@ -3,19 +3,46 @@ from __future__ import print_function
 import time
 import sys
 import math
+import re
+
+usage = 'Usage: {} TIME\n\
+    Where TIME can be expressed in a couple of ways\n\
+    10      -> 10 seconds, just a number will be interpeted as seconds\n\
+    10[MHS] -> for 10 minutes, hours or seconds\n\
+    1:10    -> for 1 hour and 10 minutes\n\
+    1:10:5  -> for 1 hour, 10 minutes and 5 seconds'
+
+if len(sys.argv) > 2:
+    print('To many arguments')
+    print(usage)
+    sys.exit(1)
 
 width = 50.0
 try:
-    counter = int(sys.argv[1])
+    sleep = int(sys.argv[1])
 except ValueError:
-    print('Use a integer value for counter')
-    sys.exit(1)
+    try:
+        timestr = sys.argv[1]
+        if re.match('\d+:\d+:\d+', timestr):
+            hours, minutes, seconds = timestr.split(':')
+            sleep = 3600*int(hours)+60*int(minutes)+int(seconds)
+        elif re.match('\d+:\d+', timestr):
+            hours, minutes = timestr.split(':')
+            sleep = 3600*int(hours)+60*int(minutes)
+        elif re.match('\d+[mhs]', timestr, re.IGNORECASE):
+            m = re.match('(\d+)([mhs])', timestr, re.IGNORECASE)
+            multiplydict = dict(s=1, m=60, h=3600)
+            sleep = int(m.group(1)) * multiplydict[m.group(2)]
+    except:
+        print('Could not parse given time')
+        print(usage)
 except IndexError:
-    print('Usage: {} counter\nwhere counter is a integer'.format(sys.argv[0]))
+    print('To few argumenst')
+    print(usage)
     sys.exit(1)
 
-for i in xrange(counter):
-    nrstars = int(width / counter * i)
+for i in xrange(sleep):
+    nrstars = int(width / sleep * i)
     print('[{}{}]'.format('*' * nrstars, ' ' * int(math.ceil(width - nrstars))), end='\r')
     sys.stdout.flush()
     time.sleep(1)
