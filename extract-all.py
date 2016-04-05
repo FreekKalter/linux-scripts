852f929c18bd5351398b22054eb07591c7bf5343
Freek Kalter <freek@kalteronline.org>
2016-04-05 18:18:11 +0200

    Make extract-all more robust

--- extract-all.py
+++ extract-all.py
@@ -1,11 +1,20 @@
-from path import path
+from path import Path
 import os
 import re
+import argparse
+import subprocess
 
-thisDir = path(os.getcwd())
-archives = re.compile("part01\.rar$")
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dir', default='.', help='path to look for rar archives')
+parser.add_argument('-t', '--to', default='.', help='path to extract content to')
 
-for f in thisDir.files():
-    if( archives.search(f.name) ):
-        if os.fork() == 0:
-            os.execlp("unrar","unrar", "x", "-ad", f.abspath())
+args = parser.parse_args()
+archive_dir = Path(os.getcwd())
+archive_re = re.compile("(part01\.rar)|(r00)$")
+
+for f in archive_dir.files():
+    if(archive_re.search(f.name)):
+        try:
+            cp = subprocess.run(["unrar", "x", "-ad", f.abspath(), Path(args.to)], stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError:
+            print(cp.stderr)
